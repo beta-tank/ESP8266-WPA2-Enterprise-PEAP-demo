f681ae3a8af3844fa37d74a7d7d0015c6c03cad0
beta-tank <beta-tank@outlook.com>
2019-01-16 21:17:33 +0700

    Update target framework branch. Format code.

--- src/main.cpp
+++ src/main.cpp
@@ -16,34 +16,33 @@ void setup() {
   delay(500);
   // WPA2 Connection starts here
   // Setting ESP into STATION mode only (no AP mode or dual mode)
-    wifi_set_opmode(STATION_MODE);
-    struct station_config wifi_config;
-    memset(&wifi_config, 0, sizeof(wifi_config));
-    strcpy((char*)wifi_config.ssid, ssid);
-    wifi_station_set_config(&wifi_config);
-    wifi_station_clear_cert_key();
-    wifi_station_clear_enterprise_ca_cert();
-    wifi_station_set_wpa2_enterprise_auth(1);
-    wifi_station_set_enterprise_identity((uint8*)username, strlen(username));
-    wifi_station_set_enterprise_username((uint8*)username, strlen(username));
-    wifi_station_set_enterprise_password((uint8*)password, strlen(password));
-    wifi_station_connect();
-
+  wifi_set_opmode(STATION_MODE);
+  struct station_config wifi_config;
+  memset(&wifi_config, 0, sizeof(wifi_config));
+  strcpy((char*)wifi_config.ssid, ssid);
+  wifi_station_set_config(&wifi_config);
+  wifi_station_clear_cert_key();
+  wifi_station_clear_enterprise_ca_cert();
+  wifi_station_set_wpa2_enterprise_auth(1);
+  wifi_station_set_enterprise_identity((uint8*)username, strlen(username));
+  wifi_station_set_enterprise_username((uint8*)username, strlen(username));
+  wifi_station_set_enterprise_password((uint8*)password, strlen(password));
+  wifi_station_connect();
   // WPA2 Connection ends here
 
   // Normal Connection starts here
   /*
-  WiFi.mode(WIFI_STA);
-  Serial.write("\r\nConnect to WLAN");
-  WiFi.begin(ssid, password);
-  // Normal Connection ends here
+    WiFi.mode(WIFI_STA);
+    Serial.write("\r\nConnect to WLAN");
+    WiFi.begin(ssid, password);
   */
+  // Normal Connection ends here
 
   // Wait for connection AND IP address from DHCP
   Serial.println();
   Serial.println("Waiting for connection and IP Address from DHCP");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(2000);
+    delay(1000);
     Serial.print(WiFi.status());
   }
   Serial.println("");
